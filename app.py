65947b81b57a1788281c6e020acaaa64b2b7d1ae
Penrose Learning <50510199+penroselearning@users.noreply.github.com>
2021-02-07 15:51:06 +0400

    New Quiz Updates

--- app.py
+++ app.py
@@ -8,10 +8,21 @@ import json
 with open('quiz.json') as file:
     quizapp = json.load(file)
 
+with open('players.json') as file:
+    players = json.load(file)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 
 
+@app.route("/",methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        session["player"] = request.form["player"].title()
+
+        return redirect(url_for('quiz'))
+
+    return render_template('index.html')
 
 @app.route("/quiz",methods=['GET','POST'])
 def quiz():
@@ -32,12 +43,14 @@ def quiz():
 
         # Starting Score
         session["score"] = 0
+        session["status"] = 1
 
         # Pick the First Question from the selected theme
         session["current_question"] = [game for game in session["game"][session["theme"]]][session["current_question_id"]-1]
 
         return render_template("start_quiz.html",
         game = session["current_question"])
+
 
     return render_template("quiz.html",
     quizapp = quizapp)
@@ -46,7 +59,7 @@ def quiz():
 @app.route('/start_quiz',methods=['GET','POST'])
 def start_quiz():
     
-    if request.method == "POST":
+    if request.method == "POST" and session["status"]==1:
         
         # Gather the answer provided by the player
         given_answer = request.form["given_answer"]
@@ -65,9 +78,31 @@ def start_quiz():
             session["current_question"] = [game for game in session["game"][session["theme"]]][session["current_question_id"]-1]
         else:
             flash("Quiz Completed. Try another Theme","completed")
+            
+            session["status"] = 0
+
+            # Store the Player details to the JSON File
+            # Player does not exist, add the new player with their score
+            if not session["player"] in [player['name'] for player in players]: 
+                player_details = {'name' : session["player"],
+                    'score' : session["score"],
+                    'theme' : session["theme"]}
+
+                players.append(player_details)
+            # Player Exists then update their score if greater than previous attempt
+            else:
+                for player in players:
+                    if player["name"] == session["player"] and player["theme"] == session["theme"]: 
+                        if player["score"] < session["score"]: player["score"] = session["score"]
+
+            # Sort the JSON File in the order of highest score
+            with open('players.json','w') as file:
+                players.sort(key=lambda k: k['score'],reverse = True) 
+                json.dump(players,file,indent = 2)
 
     return render_template("start_quiz.html",
-    game = session["current_question"])
+    game = session["current_question"],
+    players=[p for p in players if p["theme"]==session["theme"]])
 
 if __name__ == "__main__":
     app.run(debug=True)
